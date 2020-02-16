f2a413a2e38fb14ba1d3fb7d006ba221caef5865
U-AES\HAEEDU <HAEEDU@HAE-NCONF002025.aes.hkmg.global>
2020-02-17 08:49:19 +0900

    React router is applied.

--- front/src/App.js
+++ front/src/App.js
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 import NavigationDrawer from "./components/navigationDrawer/navigationDrawer";
 import "./css/common.css";
 import "@material/react-icon-button/dist/icon-button.css";
-import TransactionDetail from "./pages/TransactionDetail";
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { TransactionDetail } from './pages';
 
 function App() {
-  const [whatDrawers,setWhatDrawers] = useState("notDrawers");
-  const onSearchSubmit = (props) => {
-    if(!props) setWhatDrawers("notDrawersWhenToggled");
+  const [whatDrawers, setWhatDrawers] = useState("notDrawers");
+  const onSearchSubmit = props => {
+    if (!props) setWhatDrawers("notDrawersWhenToggled");
     else setWhatDrawers("notDrawers");
-  }
+  };
 
   return (
     <div>
@@ -18,7 +19,9 @@ function App() {
           <NavigationDrawer onSubmit={onSearchSubmit} />
         </div>
         <div className={whatDrawers}>
-          <TransactionDetail />
+          <Router>
+            <Route path="/detail" component={TransactionDetail} />
+          </Router>
         </div>
       </div>
     </div>
