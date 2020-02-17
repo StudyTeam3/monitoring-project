03389470532619e66abcd4c28ac16cbe7e810db9
U-AES\HAEEDU <HAEEDU@HAE-NCONF002025.aes.hkmg.global>
2020-02-17 11:18:28 +0900

    dashboard width changed

--- front/src/components/dashboard/Dashboard.js
+++ front/src/components/dashboard/Dashboard.js
@@ -1,48 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import FirstChart from './FirstChart';
-import SecondChart from './SecondChart';
-import ThirdChart from './ThirdChart';
-import DashboardTable from './DashboardTable';
-import DashboardLogTable from './DashboardLogTable';
+import FirstChart from "./FirstChart";
+import SecondChart from "./SecondChart";
+import ThirdChart from "./ThirdChart";
+import DashboardTable from "./DashboardTable";
+import DashboardLogTable from "./DashboardLogTable";
+import Paper from "@material-ui/core/Paper";
 
-import '../../css/dashboard.css'
-import '../../../node_modules/react-vis/dist/style.css';
+import "../../css/dashboard.css";
+import "../../../node_modules/react-vis/dist/style.css";
+import "../../css/common.css";
 
 class Dashboard extends Component {
-
-    render() {
-        return (
+  render() {
+    return (
+      <div className="dashBoard">
+          <h1 className="header">Dashboard</h1>
+          <hr
+            style={{
+              color: "#000066",
+              height: "1px",
+              background: "#00287A",
+              marginLeft: "20px",
+              marginRight: "20px"
+            }}
+          />
+          <div className="chartStyle">
             <div>
-                <h1 className="headerStyle">Dashboard</h1>
-                <hr style={{color : '#000066', height : '1px', background : '#00287A', marginLeft : '20px', marginRight : '20px'}}/>
-                <div className="chartStyle">
-                    <div>
-                        <h2>시간대별 요청 흐름</h2>
-                        <FirstChart></FirstChart>
-                    </div>
-                    <div>
-                        <h2>기능별 요청 빈도</h2> 
-                        <SecondChart></SecondChart>
-                    </div>
-                    <div>
-                        <h2>트랜잭션 성공률</h2>
-                        <ThirdChart></ThirdChart>
-                    </div>
-                </div>
-                <div className="tableStyle">
-                    <div style={{width:'35%'}}>
-                        <h2>서버 현황</h2>
-                        <DashboardTable></DashboardTable>
-                    </div>
-                    <div style={{width:'60%'}}>
-                        <h2>최근 실패 로그</h2>
-                        <DashboardLogTable style={{width:'70%'}}></DashboardLogTable>
-                    </div>
-                </div>
+              <h2>시간대별 요청 흐름</h2>
+              <FirstChart></FirstChart>
             </div>
-        );
-    }
+            <div>
+              <h2>기능별 요청 빈도</h2>
+              <SecondChart></SecondChart>
+            </div>
+            <div>
+              <h2>트랜잭션 성공률</h2>
+              <ThirdChart></ThirdChart>
+            </div>
+          </div>
+          <div className="tableStyle">
+            <div style={{ width: "35%" }}>
+              <h2>서버 현황</h2>
+              <DashboardTable></DashboardTable>
+            </div>
+            <div style={{ width: "60%" }}>
+              <h2>최근 실패 로그</h2>
+              <DashboardLogTable style={{ width: "70%" }}></DashboardLogTable>
+            </div>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
