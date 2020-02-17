cea07c9201883e37581edd7ed31596c25dd25081
U-AES\HAEEDU <HAEEDU@HAE-NCONF002025.aes.hkmg.global>
2020-02-17 10:21:28 +0900

    Strange bug is found

--- front/src/App.js
+++ front/src/App.js
@@ -1,27 +1,22 @@
-import React, { Component, setState } from "react";
-import logo from './logo.svg';
-import './App.css';
-import Dashboard from './components/dashboard/Dashboard';
+import React, { Component } from "react";
+import Dashboard from "./components/dashboard/Dashboard";
 import NavigationDrawer from "./components/navigationDrawer/navigationDrawer";
-import "./css/common.css";
 import "@material/react-icon-button/dist/icon-button.css";
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-import { TransactionDetail } from './pages';
-
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import { TransactionDetail } from "./pages";
+import "./css/common.css";
+import "./App.css";
 
 class App extends Component {
-
   state = {
-    whatDrawers : "notDrawers",
-  }
-
-  // const [whatDrawers,setWhatDrawers ] = useState("notDrawers");
+    whatDrawers: "notDrawers"
+  };
 
   onSearchSubmit = props => {
-    if (!props) this.setState({whatDrawers: "notDrawersWhenToggled"});
-    else this.setState({whatDrawers:"notDrawers"});
+    if (!props) this.setState({ whatDrawers: "notDrawersWhenToggled" });
+    else this.setState({ whatDrawers: "notDrawers" });
   };
-  
+
   render() {
     return (
       <div>
@@ -32,7 +27,7 @@ class App extends Component {
           <div className={this.whatDrawers}>
             <Router>
               <Route path="/detail" component={TransactionDetail} />
-              <Route path="/home" component={Dashboard} />
+              {/* <Route path="/home" component={Dashboard} /> */}
             </Router>
           </div>
         </div>
