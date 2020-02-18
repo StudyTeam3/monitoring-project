f07e22329de800f62bd1be9322249151223eddb6
U-AES\HAEEDU <HAEEDU@HAE-NCONF002015.aes.hkmg.global>
2020-02-18 16:37:37 +0900

    change index.js

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,4 +1,5 @@
     
 export { default as TransactionDetail } from './TransactionDetail';
 export { default as TransactionSearch } from './TransactionSearch';
+export { default as Setting } from './Setting';
 // export { default as Dashboard } from '../components/dashboard/Dashboard';
