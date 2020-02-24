381d4facbf649ae32191381f2e0c25551a8c4fb5
U-AES\HAEEDU <HAEEDU@HAE-NCONF002024.aes.hkmg.global>
2020-02-24 11:22:51 +0900

    sorted secondChart

--- front/src/components/dashboard/SecondChart.js
+++ front/src/components/dashboard/SecondChart.js
@@ -20,7 +20,6 @@ class SecondChart extends React.Component {
     // DB 데이터 파싱
     let functionCount = {};
     let { data: functions } = await axios.get(FunctionUrl);
-
     for(var i = 0; i < Object.keys(functions).length; i++){
       let functionKind = functions[i].function;
       if(functionKind != 'null'){
@@ -33,34 +32,44 @@ class SecondChart extends React.Component {
       }
     }
 
+    // 데이터 소팅
+    let sortedArray = [];
+    for(var i in functionCount){
+      sortedArray.push([functionCount[i],i]);
+    }
+    sortedArray = sortedArray.sort(function(a,b){
+      return b[0]-a[0];
+    });
+
+    // console.log("sorted",sortedArray.sort(function(a,b){
+    //   return b[0]-a[0];
+    // }));
     // 데이터를 차트 데이터로 변형
     let tempData = [];
     let currentDegree = 0;
     let allOfCount = 0;
     let loop = 0;
 
-    for(var i in functionCount){
-      allOfCount += functionCount[i];
+    for(var i in sortedArray){
+      allOfCount += sortedArray[i][0];
     }
 
     let dataName = [];
-    for(var i in functionCount){
+    for(var i in sortedArray){
       let tempJson = {};
       tempJson['angle0'] = currentDegree;
-      tempJson['angle'] = currentDegree +  PI * (functionCount[i]/allOfCount);
+      tempJson['angle'] = currentDegree +  PI * (sortedArray[i][0]/allOfCount);
       tempJson['radius'] = 1.5;
       tempJson['radius0'] = 2;
       tempJson['color'] = colorArr[loop];
       loop += 1;
       currentDegree = tempJson['angle'];
       tempData.push(tempJson);
-      let tempName = {};
-      tempName['title'] = i;
-      dataName.push(i);
+      dataName.push(sortedArray[i][1]);
     }
 
     this.setState({data : tempData, dataName: dataName});
-    console.log(this.state.dataName);
+    console.log('data',this.state.dataName);
   };
 
   componentWillMount (){
@@ -83,15 +92,24 @@ class SecondChart extends React.Component {
             data={this.state.data}
           />
         </XYPlot>
+        <table>
         {
           // index < 5 && value != 'null'
+          // this.state.dataName.map((value, index) => { if(1){
+          // return  <div>
+          //           <div style={{backgroundColor: colorArr[index], width: '10px', height: '10px', margin: '0px', display: 'inline-block', marginRight: '10px', marginLeft: '110px', marginBottom: '10px'}}></div>
+          //           <p style={{textAlign: 'center', marign: '0px', display: 'inline', marginBottom: '20px'}}>{value}</p>
+          //         </div>
+          // }})
+          
           this.state.dataName.map((value, index) => { if(1){
           return  <div>
-                    <div style={{backgroundColor: colorArr[index], width: '10px', height: '10px', margin: '0px', display: 'inline-block', marginRight: '10px', marginLeft: '110px', marginBottom: '10px'}}></div>
+                    <div style={{backgroundColor: colorArr[index], width: '10px', height: '10px', margin: '0px', display: 'inline-block', marginRight: '10px', marginLeft: '80px', marginBottom: '10px'}}></div>
                     <p style={{textAlign: 'center', marign: '0px', display: 'inline', marginBottom: '20px'}}>{value}</p>
                   </div>
           }})
         }
+        </table>
       </Fragment>
     );
   }
